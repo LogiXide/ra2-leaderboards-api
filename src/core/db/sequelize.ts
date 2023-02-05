c5e1743316b779b93fbac476d76ffc3a8b80908c
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2023-02-05 16:57:11 +0300

    refactor: refactoring sequalize

--- src/core/db/sequelize.ts
+++ src/core/db/sequelize.ts
@@ -3,13 +3,13 @@ import { Sequelize } from 'sequelize-typescript'
 import { Map } from '../../maps/db/map.js'
 import { MapPool } from '../../maps/db/map-pool.js'
 import { MapPoolMap } from '../../maps/db/map-pool-map.js'
+import config from '../../config/config.js'
+
+const env = (process.env.NODE_ENV || 'production') as keyof typeof config
+const sequelizeOptions = config[env]
 
 const sequelize = new Sequelize({
-  dialect: 'postgres',
-  username: "postgres",
-  password: "killmenot",
-  database: "postgres",
-  host: "127.0.0.1",
+  ...sequelizeOptions,
   models: [
     Map,
     MapPool,
