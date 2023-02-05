cb2231135b9f5867afd81bb73660bd2a4a33692d
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2023-02-05 18:57:06 +0300

    refactor: cleanup

--- src/maps/db/map.ts
+++ src/maps/db/map.ts
@@ -1,21 +1,9 @@
 import { Model, Table, Column, BelongsToMany } from 'sequelize-typescript'
-import { CreationOptional, InferAttributes, InferCreationAttributes } from 'sequelize'
-import sequelizeCursorPagination from 'sequelize-cursor-pagination'
-import type { PaginateOptions, PaginationConnection } from 'sequelize-cursor-pagination'
 import { MapPoolMap } from './map-pool-map.js'
 import { MapPool } from './map-pool.js'
 
-//const  = sequelizeCursorPagination
-// { PaginateOptions, PaginationConnection, makePaginate }
-
 @Table
-export class Map extends Model<
-  InferAttributes<Map>,
-  InferCreationAttributes<Map>
-> {
-  declare id: CreationOptional<number>;
-  declare value: number;
-
+export class Map extends Model {
   @Column
   name!: string
 
@@ -30,8 +18,4 @@ export class Map extends Model<
 
   @BelongsToMany(() => MapPool, () => MapPoolMap)
   mapPools!: MapPool[]
-
-  static paginate = (options: PaginateOptions<Map>) => sequelizeCursorPagination.makePaginate(Map);
 }
-
-//Map.paginate = sequelizeCursorPagination.makePaginate(Map);
--- src/maps/resolvers/maps.queries.ts
+++ src/maps/resolvers/maps.queries.ts
@@ -4,7 +4,6 @@ const mapsQueries = {
   maps: async (_: any, args: any, context: Context) => {
     const maps = await context.db.maps.findAll()
 
-
     return maps
   },
 };
