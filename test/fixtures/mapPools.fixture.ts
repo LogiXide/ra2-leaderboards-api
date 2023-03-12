d68075eae6d8722e0ec7e79a45a05b5e07cefde2
Aleksey Samokhin <lel.dude01@gmail.com>
2023-03-12 22:53:29 +0300

    test: add test for map pools

---
+++ test/fixtures/mapPools.fixture.ts
@@ -0,0 +1,28 @@
+import { Ref } from "../utils/TestDataBuilder.js"
+import { range } from "../utils/array-helpers.js"
+
+export default (models: any, ref: Ref) => {
+	const MapPool = models.Postgres_MapPools;
+
+	const mapPools = [
+    ...range(5).map(n =>
+      ({
+        name: `mapPool${n}`,
+        model: MapPool,
+        data: {
+          name: `mapPool${n}`,
+          created_at: new Date(Date.UTC(2023, 1, 1, 0, 0, 0)),
+          updated_at: new Date(Date.UTC(2023, 1, 1, 0, 0, 0)),
+        },
+      })
+    )
+	]
+
+	return {
+		require: [],
+		data: [
+			...mapPools,
+		],
+	}
+}
+
