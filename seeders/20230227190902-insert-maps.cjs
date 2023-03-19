4293a0972268f7c2e9079e9c5acf942b0f0e70d5
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2023-03-19 14:49:37 +0300

    feat(t9): update seeds

--- seeders/20230227190902-insert-maps.cjs
+++ seeders/20230227190902-insert-maps.cjs
@@ -5,8 +5,88 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('maps', [
       {
-        name: 'Heck Freeze Over',
-        spots: '4',
+        name: 'Arabian Oasis',
+        spots: '2',
+        author: 'saremGts',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Dannatch',
+        spots: '2',
+        author: 'JaladTanagra',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Divide Freezes Over (PreCaptured)',
+        spots: '2',
+        author: 'DistantQ & CnCNet QM Team',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Dry Head RA2',
+        spots: '2',
+        author: 'Westwood Studios',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Estaminia',
+        spots: '2',
+        author: '[RU]Polye',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Glowing Waters',
+        spots: '2',
+        author: 'zeiZe',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Landmarks',
+        spots: '2',
+        author: 'zeiZe',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Little Big Lake LE',
+        spots: '2',
+        author: 'Westwood, Jalad & Lucifer',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Morningtide (TL vs BR)',
+        spots: '2',
+        author: 'DistantQ',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Narrow Strip Of Water',
+        spots: '2',
+        author: 'prry',
+        image_url: 'https://tempuri.org',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Official Tournament B',
+        spots: '2',
         author: 'Westwood Studios',
         image_url: 'https://tempuri.org',
         created_at: new Date(),
--- seeders/20230312210022-insert-map-pools.cjs
+++ seeders/20230312210022-insert-map-pools.cjs
@@ -5,7 +5,42 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('map_pools', [
       {
-        name: 'MapPool1',
+        name: 'Red Alert 2 1vs1',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Red Alert 2 2vs2',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Red Alert 2 3vs3',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Red Alert 2 4vs4',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Yuri\'s Revenge 1vs1',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Yuri\'s Revenge 2vs2',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Yuri\'s Revenge 3vs3',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Yuri\'s Revenge 4vs4',
         created_at: new Date(),
         updated_at: new Date()
       },
