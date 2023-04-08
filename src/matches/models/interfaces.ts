83e0887a3ab099d3a7ecdcac2f6cd88748952d5c
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2023-04-08 13:10:48 +0300

    chore(t53): lint

--- src/matches/models/interfaces.ts
+++ src/matches/models/interfaces.ts
@@ -1,29 +1,29 @@
 import { Optional } from 'sequelize'
 
 export interface IGameAttributes {
-  id: number;
-  type: string;
-  winner: string;
-  matchId: number;
+  id: number
+  type: string
+  winner: string
+  matchId: number
   mapId: number
-  homePlayerId?: number;
-  homeTeamId?: number;
-  awayPlayerId?: number;
-  awayTeamId?: number;
-  createdAt: Date;
-  updatedAt: Date;
+  homePlayerId?: number
+  homeTeamId?: number
+  awayPlayerId?: number
+  awayTeamId?: number
+  createdAt: Date
+  updatedAt: Date
 }
 
 export interface IMatchAttributes {
-  id: number;
-  type: string;
-  winner?: string;
-  homePlayerId?: number;
-  homeTeamId?: number;
-  awayPlayerId?: number;
-  awayTeamId?: number;
-  createdAt: Date;
-  updatedAt: Date;
+  id: number
+  type: string
+  winner?: string
+  homePlayerId?: number
+  homeTeamId?: number
+  awayPlayerId?: number
+  awayTeamId?: number
+  createdAt: Date
+  updatedAt: Date
 }
 
 export interface IGameCreationAttributes extends Optional<IGameAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
